a7d2e82eb2af3f9b93979dd14696a241e097c1a6
bhavana <bhavana.chinthalapally@gmail.com>
2022-02-02 04:32:31 +0530

    feat: added carousel in landing page

--- frontend/gator-desk/src/app/app.module.ts
+++ frontend/gator-desk/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,11 +11,14 @@ import {MatIconModule,  MatButtonModule, MatButtonToggleModule, } from "@angular
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {IvyCarouselModule} from 'angular-responsive-carousel';
 import {MatCardModule} from '@angular/material/card';
+import { AnnouncementComponent } from './announcement/announcement.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AnnouncementComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +30,8 @@ import {MatCardModule} from '@angular/material/card';
     BrowserModule,
     BrowserAnimationsModule,
     IvyCarouselModule,
-    MatCardModule
+    MatCardModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/gator-desk/src/app/home/home.component.ts
+++ frontend/gator-desk/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { 
+    
+  }
+
+  public imageSources = [
+    {path: 'https://ak9.picdn.net/shutterstock/videos/5117969/thumb/1.jpg'},
+    {path: 'http://ak2.picdn.net/shutterstock/videos/18324082/thumb/3.jpg'},
+    {path:'https://ak8.picdn.net/shutterstock/videos/3159418/thumb/7.jpg?i10c=img.resize(height:160)'},
+    {path:'https://ak8.picdn.net/shutterstock/videos/11062748/thumb/1.jpg'},
+    {path:'https://ak1.picdn.net/shutterstock/videos/8221801/thumb/1.jpg'}
+  ];
+
+  public config: IvyCarouselModule = {
+    animation: true,
+    autoplay: true,
+    autoplayDelay: 2000,
+    stopAutoplayMinWidth: 768
+  };
+
+  navigate(){
+    this.router.navigate(['annoucement']);
+    }
+ 
 
   ngOnInit(): void {
   }
