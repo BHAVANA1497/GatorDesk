0a09a8eec7b6f0cae78916e65eed6fcca2d77684
Nitya Vaishnavi Mamillapalle <nityavaishnavi@Nityas-MacBook-Air.local>
2022-02-03 17:01:49 -0500

    feature: createAnnouncement API

--- backend/webapp/main.go
+++ backend/webapp/main.go
@@ -1,13 +1,42 @@
 package main
 
-import "github.com/gin-gonic/gin"
+// Imports Gin, Gorm, models and views
+import (
+	m "webapp/model"
+	a "webapp/views"
 
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+// Driver function which starts the server
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	// Connection to the database with default configuration
+	db, err := gorm.Open(sqlite.Open("main.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the Announcement model to the db
+	db.AutoMigrate(&m.Announcement{})
+
+	// setting up the webserver with default config
+	r := gin.New()
+
+	// Adding logger to the middleware
+	//r.Use(gin.Logger())
+
+	// Using default recovery mechanism in case of any unexpected crashes in webserver
+	//r.Use(gin.Recovery())
+
+	// **** END POINTS ****
+	//r.GET("/listAllAnouncements", a.ListAnnouncementsView(db))
+	//r.POST("/editAnnoucements/{id}", a.EditAnnouncementView(db))
+	r.POST("/createAnnoucement", a.CreateAnnouncementView(db))
+	//r.POST("/deleteAnnoucement/{id}", a.DeleteAnnouncementView(db))
+
+	// starts server and listens on port 8080
+	r.Run(":8181")
 }
