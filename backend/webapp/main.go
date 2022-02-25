8796671bb38bdbf0f3b08335e0ecd227c960149e
Nitya Vaishnavi Mamillapalle <nithyammllppll@gmail.com>
2022-02-25 18:36:42 -0500

    SignUp/API Created

--- backend/webapp/main.go
+++ backend/webapp/main.go
@@ -10,6 +10,20 @@ import (
 	"gorm.io/gorm"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
+
 // Driver function which starts the server
 func main() {
 
@@ -19,11 +33,17 @@ func main() {
 		panic("failed to connect database")
 	}
 
+	// Migrate the User model to the db
+	db.AutoMigrate(&m.User{})
+
 	// Migrate the Announcement model to the db
 	db.AutoMigrate(&m.Announcement{})
 
 	// setting up the webserver with default config
 	r := gin.New()
+
+	// Check every request if allowed for CORS
+	r.Use(CORSMiddleware())
 
 	// Adding logger to the middleware
 	//r.Use(gin.Logger())
@@ -32,6 +52,8 @@ func main() {
 	//r.Use(gin.Recovery())
 
 	// **** END POINTS ****
+	//r.POST("/login", a.LoginView(db))
+	r.POST("/signUp", a.SignUpView(db))
 	r.GET("/listAllAnnouncements", a.ListAnnouncementsView(db))
 	r.PUT("/editAnnouncement", a.EditAnnouncementView(db))
 	r.POST("/createAnnouncement", a.CreateAnnouncementView(db))
