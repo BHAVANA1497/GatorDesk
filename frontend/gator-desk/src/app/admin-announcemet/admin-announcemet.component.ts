5536b9bf97de227ec9ebea13fd5c37fd66454e18
bhavana <bhavana.chinthalapally@gmail.com>
2022-02-28 14:47:34 -0500

    added login component

--- frontend/gator-desk/src/app/admin-announcemet/admin-announcemet.component.ts
+++ frontend/gator-desk/src/app/admin-announcemet/admin-announcemet.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AdminAnnouncemetService } from './admin-annoucemet.admin';
 
 @Component({
   selector: 'app-admin-announcemet',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-announcemet.component.css']
 })
 export class AdminAnnouncemetComponent implements OnInit {
+  announcementForm: FormGroup;
+  eventTitle = '';
+  eventCategory = '';
+  eventDesc = '';
+  postData: any = {};
 
-  constructor() { }
+  constructor(private fb: FormBuilder, public  admObj:AdminAnnouncemetService,private router: Router, private activatedRoute: ActivatedRoute ) { 
+    this.announcementForm = fb.group({
+      'eventTitle': [null, Validators.required],
+      'eventDesc': [null, Validators.required]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  selectedValue(selectedValue: any){
+    this.eventCategory = selectedValue.value;
+  }
+
+  onSubmit(){
+
+    this.eventTitle = this.announcementForm.value.eventTitle;
+    this.eventDesc = this.announcementForm.value.eventDesc;
+    console.log( this.eventTitle + " "+ this.eventCategory + " " + this.eventDesc);
+    this.postData.announcementTitle = this.announcementForm.value.eventTitle;
+    this.postData.eventDescription = this.announcementForm.value.eventDesc;
+    this.postData.announcementCategory = this.eventCategory;
+
+    this.admObj.createAnnouncement(this.postData);
+  
+   
   }
 
 }
---
+++ frontend/gator-desk/src/app/login/login.component.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../providers/user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  userData = {
+    email: '',
+    password: '',
+  };
+  constructor(private _http: HttpClient, public user: UserService,  private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  doLogin() {
+    this.router.navigate(['/home']);
+    // this._http.post('users/login', this.userData)
+    //   .subscribe(res => {
+    //     console.log(res);
+    //     // this.user._details = res['data'];
+    //     // this.user._token = res['token'];
+    //     // localStorage.setItem('TOKEN', res['token']);
+    //     // localStorage.setItem('USER_DETAILS', JSON.stringify(res['data']));
+    //     this.router.navigate(['/home']);
+    //     // alert(res.message);
+    //   }, err => {
+    //     console.log(err);
+    //     alert(JSON.parse(err._body).message);
+    //   });
+  }
+
+}
