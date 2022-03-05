f33e3a1915282d3ba9cbd07f3ebb8d47c2940eca
HarshithaMyadam <myadamharshitha@gmail.com>
2022-03-04 22:54:01 -0500

    added test cases for jest annoucement component

--- frontend/gator-desk/src/app/admin-announcemet/admin-announcement.component.spec.ts
+++ frontend/gator-desk/src/app/admin-announcemet/admin-announcement.component.spec.ts
@@ -1,30 +1,14 @@
-import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AdminAnnouncementService } from './admin-annoucement.service';
-import {
-  HttpClientTestingModule,
-  HttpTestingController
-} from '@angular/common/http/testing';
-
 
 import { AdminAnnouncementComponent } from './admin-announcement.component';
 
 describe('AdminAnnouncementComponent', () => {
   let component: AdminAnnouncementComponent;
   let fixture: ComponentFixture<AdminAnnouncementComponent>;
-  let injector: TestBed;
-  let service: AdminAnnouncementService;
-  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AdminAnnouncementComponent ],
-      imports: [HttpClientModule, FormsModule, ReactiveFormsModule, BrowserModule , RouterModule, RouterTestingModule],
-      providers: [AdminAnnouncementService]
+      declarations: [ AdminAnnouncementComponent ]
     })
     .compileComponents();
   });
@@ -38,5 +22,4 @@ describe('AdminAnnouncementComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
 });
