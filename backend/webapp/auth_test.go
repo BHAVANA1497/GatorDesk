3342102ecb62ac9388bc89b9670a261813b65879
Akhil Maddukuri <maddukuri.akhil0803@gmail.com>
2022-03-04 22:56:28 -0500

    Singup test cases

--- backend/webapp/auth_test.go
+++ backend/webapp/auth_test.go
@@ -51,3 +51,29 @@ func TestLoginFailCase(t *testing.T) {
 	assert.Equal(t, 401, w.Code)
 
 }
+
+func TestRegisterPassCase(t *testing.T) {
+	user_details := m.User{
+		Username:  "Vamsee",
+		Password:  "nothing",
+		FirstName: "Vamsee",
+		LastName:  "Raj",
+		Phone:     "8928954545",
+		AptNo:     "B-1333",
+	}
+	body, err := json.Marshal(user_details)
+	if err != nil {
+		t.Fatal(err)
+	}
+	w := httptest.NewRecorder()
+
+	req, err := http.NewRequest("POST", "/signUp", strings.NewReader(string(body)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+
+}
