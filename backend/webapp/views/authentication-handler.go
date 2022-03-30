2ff7f304296447d320fddcff53bdeed66d30127a
Akhil Maddukuri <maddukuri.akhil0803@gmail.com>
2022-03-30 03:06:34 -0400

    Admin Login created

--- backend/webapp/views/authentication-handler.go
+++ backend/webapp/views/authentication-handler.go
@@ -163,3 +163,44 @@ func LogoutView(c *gin.Context) {
 		"result": "Logout failed",
 	})
 }
+
+func AdminLoginView(db *gorm.DB) gin.HandlerFunc {
+	fn := func(c *gin.Context) {
+		session := sessions.Default(c)
+		var json m.Login
+		// try to bind the request json to the Login struct
+		if err := c.ShouldBindJSON(&json); err != nil {
+			// return bad request if field names are wrong
+			// and if fields are missing
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		var admins []m.Admin
+
+		// strips HTML input from strings preventing XSS
+		p := bluemonday.StripTagsPolicy()
+		username := p.Sanitize(json.Username)
+		password := p.Sanitize(json.Password)
+
+		// DB query to search for username and password and store the results in users
+		db.Find(&admins, "username = ? AND password = ?", username, password)
+
+		// if user found return success
+		if len(admins) > 0 {
+			session.Set("uId", admins[0].ID)
+			session.Save()
+			c.JSON(http.StatusOK, gin.H{
+				"result": "Successful Login!",
+			})
+			return
+		}
+
+		// return unauthorized status
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"result": "Invalid Admin name and/or Password",
+		})
+	}
+
+	// return the loginHandlerfunction
+	return gin.HandlerFunc(fn)
+}
