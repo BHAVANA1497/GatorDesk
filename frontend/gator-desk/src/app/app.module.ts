7db8d95b7684cac194d62e60107fb4ae541ef5f2
bhavana <bhavana.chinthalapally@gmail.com>
2022-04-01 16:29:30 -0400

    packages in json

--- frontend/gator-desk/src/app/app.module.ts
+++ frontend/gator-desk/src/app/app.module.ts
@@ -29,6 +29,7 @@ import{MatFormFieldModule} from'@angular/material/form-field';
 import { MatSelectModule} from '@angular/material/select';
 import {    MatInputModule} from '@angular/material/input';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDividerModule } from '@angular/material/divider';
 
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MatOptionModule } from '@angular/material/core';
@@ -53,7 +54,7 @@ import { SignupComponent } from './signup/signup.component';
     MaintenanceRequestComponent,
     AdminAnnouncementComponent,
     LoginComponent,
-    // AdminLostAndFoundComponent,
+    AdminLostAndFoundComponent,
     SignupComponent,
   ],
   imports: [
@@ -78,6 +79,7 @@ import { SignupComponent } from './signup/signup.component';
     MatIconModule,
     MatOptionModule,
     HttpClientModule,
+    MatDividerModule
   ],
   providers: [
     AdminAnnouncementService,
