1541d7d75176de94d867ed8cdbfebf1f15bbc470
bhavana <bhavana.chinthalapally@gmail.com>
2022-04-01 21:32:09 -0400

    added cypress for lost and found

--- frontend/gator-desk/cypress/integration/home.test.js
+++ frontend/gator-desk/cypress/integration/home.test.js
@@ -34,3 +34,24 @@ it('tests navigation to Lost & Found component' , () => {
   cy.get('#lostandfound').click();
   cy.contains('Lost');
 });
+
+it('tests navigation to Lost & Found component' , () => {
+  cy.get('#maintenancerequest').click();
+  cy.contains('Lost Request');
+});
+
+it('Fill the annoucement details, post and check if it is getting posted', () => {
+  cy.wait(2000)
+  cy.get("mat-form-field input").get(`[formcontrolname="losttype"]`).type("test lost type"); 
+  cy.wait(2000)
+
+   cy.get('#mat-select-0').click().get('mat-option').contains('LOST').click();
+   cy.wait(1000)
+  cy.get("mat-form-field input").get(`[formcontrolname="desc"]`).type("test test"); 
+  cy.wait(2000)
+  cy.get("mat-form-field input").get(`[formcontrolname="details"]`).type("test test"); 
+  cy.wait(2000)
+  cy.get('.mat-button-wrapper').contains('Create Request').click()
+  cy.wait(1000)
+  cy.contains('Found')
+})
